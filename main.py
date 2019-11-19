68c2804ed83a1743eaab31283d25e3e870f37fe5
Joshua Jung <tojjung01@gmail.com>
2019-11-18 18:43:07 -0600

    heroku commit pt.2

--- main.py
+++ main.py
@@ -29,36 +29,35 @@ def deleteMessage(messages, course):
         if course in message.body:
             client.messages(message.sid).delete()
 
-if __name__ == '__main__':
-    accountSID = os.environ["accountSID"]
-    auth_token = os.environ["auth_token"]
-    joshuapn = os.environ["joshuapn"]
-    seungjehpn = os.environ["seungjehpn"]
+accountSID = os.environ["accountSID"]
+auth_token = os.environ["auth_token"]
+joshuapn = os.environ["joshuapn"]
+seungjehpn = os.environ["seungjehpn"]
 
-    client = Client(accountSID, auth_token)
-    pn = [joshuapn, seungjehpn,]
-    for num in pn:
-        messages = client.messages.list(from_= num, limit=200)
+client = Client(accountSID, auth_token)
+pn = [joshuapn, seungjehpn,]
+for num in pn:
+    messages = client.messages.list(from_= num, limit=200)
 
-        deletedClass = findDeleteClassInMessage(messages)
-        if deletedClass != "NONE TO DELETE":
-            deleteMessage(messages, deletedClass)
-            client.messages.create(to=num,
-                                   from_="+13343104801",
-                                   body="No longer searching for " + deletedClass)
+    deletedClass = findDeleteClassInMessage(messages)
+    if deletedClass != "NONE TO DELETE":
+        deleteMessage(messages, deletedClass)
+        client.messages.create(to=num,
+                               from_="+13343104801",
+                               body="No longer searching for " + deletedClass)
 
-        desiredClasses = addMessages(messages)
-        notifiedClasses = []
+    desiredClasses = addMessages(messages)
+    notifiedClasses = []
 
-        for mess in desiredClasses:
-            if search(mess.dept, mess.num, mess.section):
-                notifiedClasses.append(mess.printName())
+    for mess in desiredClasses:
+        if search(mess.dept, mess.num, mess.section):
+            notifiedClasses.append(mess.printName())
 
-        if len(notifiedClasses) != 0:
-            body = ""
-            for course in notifiedClasses:
-                body += "\n"+course
-            client.messages.create(to=num,
-                                   from_="+13343104801",
-                                   body="These Classes are open for registration: "+body)
+    if len(notifiedClasses) != 0:
+        body = ""
+        for course in notifiedClasses:
+            body += "\n"+course
+        client.messages.create(to=num,
+                               from_="+13343104801",
+                               body="These Classes are open for registration: "+body)
 
